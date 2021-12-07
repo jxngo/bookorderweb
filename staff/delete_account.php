d310d96df6549e6018b8f7af32e90505c51efe1a
Andrew <martyn.jandrew@gmail.com>
2021-12-07 10:07:56 -0500

    update

--- staff/delete_account.php
+++ staff/delete_account.php
@@ -5,7 +5,8 @@ require_once "../utils/db_connect.php";
 
 $sql = "DELETE FROM users WHERE email='". $_SESSION['account_email'] . "'";
 if($conn->query($sql)) {
-    header("location: manage_faculty.php");
+    print "test";
+    $conn->close();
+    #header("location: manage_faculty.php");
 }
-$conn->close();
 ?>
--- staff/manage_bookrequests.php
+++ staff/manage_bookrequests.php
@@ -15,7 +15,30 @@ session_start()
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-
+<style>
+    .login-form {
+        width: 340px;
+        margin: 50px auto;
+        font-size: 15px;
+    }
+    .login-form form {
+        margin-bottom: 15px;
+        background: #f7f7f7;
+        box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+        padding: 30px;
+    }
+    .login-form h2 {
+        margin: 0 0 15px;
+    }
+    .form-control, .btn {
+        min-height: 38px;
+        border-radius: 2px;
+    }
+    .btn {        
+        font-size: 15px;
+        font-weight: bold;
+    }
+</style>
 </head>
 
 <body>
@@ -56,8 +79,19 @@ session_start()
     </div>
 </nav>
 
-<div class="jumbotron text-center">
-    <h1 class="display-4">Manage Book request Page</h1>
+<div class="login-form">
+    <form action="index.php" method="post">
+        <h2 class="text-center">Reset Password</h2>        
+        <div class="form-group">
+            <input type="password" name="new_password" class="form-control" placeholder="New Password" required="required">
+        </div>
+        <div class="form-group">
+            <input type="password" name="confirm_password" class="form-control" placeholder="Confirm New Password" required="required">
+        </div>
+        <div class="form-group">
+            <button type="submit" class="btn btn-primary btn-block">Reset</button>
+        </div>   
+    </form>
 </div>
 
 </body>
